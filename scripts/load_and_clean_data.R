315ac2a47d10616e3b3dd0647c0f381c6dad86f9
Morgan Fleming <fleming4@bu.edu>
2024-03-22 15:00:24 -0400

    combining datasets into one cleaned file and changing the clean_data script

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,35 +1,27 @@
-#library(tidyverse)
-#library(dplyr)
-#library(collapse)
-
-#going to ask prof if I should upload the original files just b/c they are
-#extremely large (like 500 MB)
-
-## LOAD the data
+library(tidyverse)
+library(dplyr)
+library(collapse)
 
 
-## CLEAN the data
+cov_collapse <- seda_cov_geodist_long_4_1 %>% 
+  drop_na(sedaleaname) %>% 
+  collap(perspeced + totenrl + perell + perfl + unempall + pernam + perasn + perblk
+         + perhsp + perwht + single_momall + snapall + unempall 
+         + snapall + perecd ~ sedaleaname + year + stateabb, FUN = list(fmean))
 
-#we will probably want to do more than this, but this is just a start
 
-# cov_collapse <- seda_cov_geodist_long_4_1 %>% 
-#   drop_na(sedaleaname) %>% 
-#   collap(perspeced + totenrl + perell + perfl + unempall + pernam + perasn + perblk
-#          + perhsp + perwht + single_momall + snapall + unempall 
-#          + snapall + perecd ~ sedaleaname + year + stateabb, FUN = list(fmean))
-# 
-# 
-# write.csv(cov_collapse, file = "SEDA_covariates_edited.csv")
-# 
-# test_scores_collapse <- seda_geodist_long_gcs %>% 
-#   drop_na(sedaleaname) %>% 
-#   collap(gcs_mn_all + gcs_mnse_all + totgyb_all + totgyb_asn + totgyb_blk + totgyb_fem
-#          +totgyb_hsp + totgyb_mal + totgyb_ecd + totgyb_mtr + totgyb_nec + 
-#            totgyb_wht + gcs_mn_asn + gcs_mn_blk + gcs_mn_ecd + gcs_mn_fem + 
-#            gcs_mn_hsp + gcs_mn_wht + gcs_mn_mal + gcs_mn_mtr
-#          ~ sedaleaname + year + stateabb + subject, FUN = list(fmean))
-# 
-# write.csv(test_scores_collapse, file = "SEDA_testscores_edited.csv")
+test_scores_collapse <- seda_geodist_long_gcs %>% 
+  drop_na(sedaleaname) %>% 
+  collap(gcs_mn_all + gcs_mnse_all + totgyb_all + totgyb_asn + totgyb_blk + totgyb_fem
+         +totgyb_hsp + totgyb_mal + totgyb_ecd + totgyb_mtr + totgyb_nec + 
+           totgyb_wht + gcs_mn_asn + gcs_mn_blk + gcs_mn_ecd + gcs_mn_fem + 
+           gcs_mn_hsp + gcs_mn_wht + gcs_mn_mal + gcs_mn_mtr
+         ~ sedaleaname + year + stateabb + subject, FUN = list(fmean))
 
+
+
+cleaned_seda <- merge(test_scores_collapse, cov_collapse, by=c("sedaleaname","year"))
+
+saveRDS(cleaned_seda, file = "cleaned_seda.rds")
 
 
