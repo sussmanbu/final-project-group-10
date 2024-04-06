45aee120dfbed30faabf88a424e7403930a82d2d
YixiaoLi0922 <ian0922@bu.edu>
2024-04-05 21:22:30 -0400

    Built site for gh-pages

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -22,6 +22,16 @@ test_scores_collapse <- seda_geodist_long_gcs %>%
 
 cleaned_seda <- merge(test_scores_collapse, cov_collapse, by=c("sedaleaname","year"))
 
+
+cleaned_seda <- cleaned_seda %>% 
+  rename(district = sedaleaname, state = stateabb, mn_score_all = cs_mn_all, 
+         mn_score_wht = cs_mn_wht, mn_score_asn = cs_mn_asn, mn_score_blk = cs_mn_blk,
+         mn_score_ecd = cs_mn_ecd, mn_score_mtr = cs_mn_mtr, mn_score_hsp = cs_mn_hsp,
+         mn_score_fem = cs_mn_fem, mn_score_male = cs_mn_mal, numstu_all = totgyb_all,
+         numstu_asn = totgyb_asn, numstu_wht = totgyb_wht, numstu_hsp = totgyb_hsp,
+         numstu_blk = totgyb_blk, numstu_fem = totgyb_fem, numstu_ecd = totgyb_ecd,
+         numstu_mtr = totgyb_mtr, numstu_male = totgyb_mal, numstu_nec = totgyb_nec)
+
 saveRDS(cleaned_seda, file = "cleaned_seda.rds")
 
 
